4c80971deb378bdddfa2b30ee09e2f5942ee163b
Kyra Lee <500001736@my.fiveislands.uwi.edu>
2024-11-27 21:03:46 -0400

    modified code

--- world.js
+++ world.js
@@ -1,15 +1,15 @@
 document.addEventListener("DOMContentLoaded", function() {
 	const searchCountry = document.getElementById("country");
 	const lookup = document.getElementById("lokup");
-	const resultDiv = dovument.getElementById("result");
+	const resultDiv = document.getElementById("result");
+	const lookupCities = document.getElementById("lookup_cities");
 
-	lookup.addEventListener("click", function() {
-		//event.preventDefault();
-		const searchedInput = searchCountry.value.trim();
+	function getResults(type) {
+		const searchedInput = searchCountry.value.trim(): '';
 		
 		const httpReq = new XMLHttpRequest();
 		
-		const url = "world.php";
+		const url = "world.php?country=${encodeURIComponent(country)}&lookup=${type}";
 		httpReq.onreadystatechange = lookup;
 		httpReq.open('GET'. url);
 		httpReq.send();
@@ -23,35 +23,11 @@ document.addEventListener("DOMContentLoaded", function() {
 				}
 			};
 		}
-	});
+	}
+	
+	lookup.addEventListener("click",()=> getResults("countries"));
+	
+	lookup_cities.addEventListener("click",()=> getResults("cities"));
 });
 		
-		/*if (!country) {
-			resultDiv.innerHTML = "<p>Please enter a country name.</p>";
-			return;
-		}
 		
-		fetch("world.php")
-			.then(response => {
-				if (response.ok){
-					return response.text()
-					//return response.json()
-				}else {
-					return Promise.reject("Country not found");
-					//return throw new Error("Country not found");
-				}
-			})
-			.then(data => {
-				result.innerHTML = data
-				//RESULT(data);
-			})
-			.catch(error => {
-				resultDiv.innerHTML = `<p>There was an error.</p>`;
-			});
-	});
-});
-
-function RESULT(data) {
-	resultDiv.innerHTML = `
-		<h2>Country Info</h2>
-		<p><bold>*/
--- world.php
+++ world.php
@@ -10,30 +10,56 @@ try{
 	
 	if (isset($_GET['country'])) {
 		$country = $_GET['country'];
+		$lookup = $_GET['lookup'] ?? 'countries';
 		
-		$stmt = $conn->prepare("SELECT * FROM countries WHERE name LIKE '%$country%'");
+		if($lookup === 'cities'){
+		
+			$stmt = $conn->prepare("SELECT cities.name AS city_name, cities.district, cities.popuation FROM cities
+									JOIN countries ON cities.country_code = countries.code WHERE countries.name LIKE "%$country%" ");
+		} else {
+			$stmt = $conn->prepare("SELECT name, continent, independence_year, head_of_state FROM countries WHERE name LIKE "%$country%" ");
+		}
+			
 		$stmt->execute();
 
 		$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 		
-		if (results) {
-			echo "<table>";
-			echo "<tr>
-					<th>Country Name</th>
-					<th>Continent</th>
-					<th>Independence Year</th>
-					<th>Head of State</th>
-				</tr>";
-			
-			foreach ($results as $row) {
-				echo "<tr>
-						<td>".htmlspecialchars(row['name']). "</td>
-						<td>".htmlspecialchars(row['continent']). "</td>
-						<td>".htmlspecialchars(row['independence_year']). "</td>
-						<td>".htmlspecialchars(row['head_of_state']). "</td>
+		if ($results) {
+			if ($lookup === "cities") {
+				echo "<table>;
+					<tr>
+						<th>Name</th>
+						<th>District</th>
+						<th>Population</th>
 					</tr>";
+				
+				foreach ($results as $row) {
+					echo "<tr>
+							<td>".htmlspecialchars($row['name']). "</td>
+							<td>".htmlspecialchars($row['district']). "</td>
+							<td>".htmlspecialchars($row['population']). "</td>
+						</tr>";
+				}
+				echo "</table>";
+			} else {
+				echo "<table>
+					<tr>
+						<th>Country Name</th>
+						<th>Continent</th>
+						<th>Independence Year</th>
+						<th>Head of State</th>
+					</tr>";
+				
+				foreach ($results as $row) {
+					echo "<tr>
+							<td>".htmlspecialchars($row['name']). "</td>
+							<td>".htmlspecialchars($row['continent']). "</td>
+							<td>".htmlspecialchars($row['independence_year']). "</td>
+							<td>".htmlspecialchars($row['head_of_state']). "</td>
+						</tr>";
+				}
+				echo "</table>";
 			}
-			echo "</table>";
 		}
 	}
 } catch (Exception $e) {
